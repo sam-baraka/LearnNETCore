5449a004f690f0330572a2349e18ce74b2088f49
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-01-23 18:58:21 +0300

    Changed naespace to project

--- LearnNETCore/Models/TodoItem.cs
+++ LearnNETCore/Models/TodoItem.cs
@@ -1,4 +1,9 @@
-﻿namespace TodoApi.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnNETCore.Models
 {
     public class TodoItem
     {
